e7abe130bffc987b68c418cf0fdb78cbdbb42590
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2022-02-24 11:52:44 +0530

    Added styles

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const todosRoutes = require('./routes/todos');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/todos', todosRoutes);
 
